54402babdf69d0013fcffdf7bbe5d42a177560b4
issam-mustapha <issamadoch777@gmail.com>
2024-12-29 14:12:43 +0100

    voici les modifications

--- main/java/loupe/Rosseta.java
+++ main/java/loupe/Rosseta.java
@@ -2,15 +2,15 @@ package loupe;
 
 public class Rosseta {
     public String change(String chaine){
-       String chaine1=chaine;
+       String chaine2=chaine;
        int longeur=chaine.length();
        if(longeur>=2){
            String racine= chaine.substring(0,longeur-2);
            char dernier = chaine.charAt(longeur-1);
            char avant_dernier=chaine.charAt(longeur-2);
-           chaine1= racine+dernier+avant_dernier;
+           chaine2= racine+dernier+avant_dernier;
        }
-       return chaine1;
+       return chaine2;
     }
 
 
